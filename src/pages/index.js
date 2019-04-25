22cb9e9bff9b02516988f756f79de59993dd3a4f
Ozzy Cardenas <ocardenas@apple.com>
2019-04-25 11:20:01 -0700

    image size fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,10 @@ import React from "react";
 import { Link } from "gatsby";
 import cx from "classnames";
 
-import Layout from "../components/layout"
-import styles from "./styles.module.css"
+import Layout from "../components/layout";
+import styles from "./styles.module.css";
 import image1 from "../images/1.png";
+import Plant from "../components/image-components/eucalyptus";
 // import SEO from "../components/seo"
 
 const IndexPage = () => (
@@ -12,7 +13,10 @@ const IndexPage = () => (
     <div className={cx(styles.wrapper, "container")}>
       <div className="">
         <div className={cx(styles.mainText, "text-center")}>Hold your horses. The wedding website will be up soon.</div>
-        <img className={cx(styles.image, "mx-auto")} src={image1}/>
+        {/* <img className={cx(styles.image, "mx-auto")} src={image1}/> */}
+        <div className={cx(styles.image, "mx-auto")}>
+          <Plant />
+        </div>
         <div className={cx(styles.subText,"text-center")}>Ozzy and Alanna</div>
       </div>
     </div>
