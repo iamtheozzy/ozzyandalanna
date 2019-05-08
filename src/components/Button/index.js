f7bba68742e27a204256180e43d2e7d84d9e2770
Ozzy Cardenas <ocardenas789@me.com>
2019-05-08 08:30:43 -0700

    added button component

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import cx from "classnames";
 
-const Button = (props) => {
+import styles from "./styles.module.scss";
+
+const Button = ({props, children}) => {
   return (
     <React.Fragment>
-      
+      <a className={cx(styles.button)} href={props.url} target="_blank" rel="noopener noreferrer">{children}</a>
     </React.Fragment>
   );
 }
--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import cx from "classnames";
-
+import Button from "../Button";
 import styles from "./styles.module.scss";
+
 
 const Hero = () => {
   return (
@@ -19,7 +20,10 @@ const Hero = () => {
               <div className={cx(styles.address)}>260 s washington st <br /> carpentersville, il 60619</div>
             </div>
           </div>
-         
+          <div className="col-lg-8 offset-lg-2 mb-4 text-center">
+            <Button url="https://ozzyandalanna.app.rsvpify.com">RSVP</Button>
+            <Button href="#" rel="noopener noreferrer">Registry</Button>
+          </div>
         </div>
       </div>
     </section>
