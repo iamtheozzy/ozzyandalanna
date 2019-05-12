148a1c593231a2a1b8d228e5c30ffd46bdbc28fd
Ozzy Cardenas <ocardenas789@me.com>
2019-05-11 18:28:05 -0700

    added registry and fixed hero page bug

---
+++ src/components/Registry/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import cx from "classnames";
+import styles from "./styles.module.scss";
+
+// Registry Image componets
+import Amazon from "../image-components/AmazonLogo";
+import BedBathBeyond from "../image-components/BBBLogo";
+import Target from "../image-components/TargetLogo";
+
+const RegistryPage = () => {
+  return (
+    <div className={cx("container",styles.wrapper)}>
+      <h1 className="text-center">REGISTRY</h1>
+      <p className="text-center col-lg-8 offset-lg-2">In consequat ipsum Lorem nisi fugiat elit elit enim amet sunt anim dolore pariatur elit. Lorem deserunt qui laboris enim consectetur dolor magna excepteur aute esse mollit qui excepteur.</p>
+      <div className={cx("col-lg-10 offset-lg-1",styles.imageContainer)}>
+        <a href="https://www.amazon.com/wedding/alanna-musci-ozzy-cardenas-chicago-july-2019/registry/30QV82R8ZXMRU" target="__blank" rel="noopener"><Amazon /></a>
+        <a href="https://www.bedbathandbeyond.com/store/giftregistry/viewregistryguest/547368787" target="__blank" rel="noopener"><BedBathBeyond /></a>
+        <a href="https://www.target.com/gift-registry/giftgiver?registryId=9fd3139498d94e12b9a938be7182f37c&type=WEDDING" target="__blank" rel="noopener"><Target /></a>
+      </div>
+    </div>
+  );
+};
+
+export default RegistryPage;
