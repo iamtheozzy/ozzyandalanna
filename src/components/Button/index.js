f19ad37d47bd4258e8c0a586d1459b1f5a5a1ef9
Ozzy Cardenas <ocardenas@apple.com>
2019-05-15 00:45:59 -0700

    deploymeny bug fix

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -7,11 +7,7 @@ const Button = ({className, children, ...rest}) => {
   const classnames = `${styles.button} ${className}`
   return (
     <React.Fragment>
-<<<<<<< HEAD
       <a className={cx(classnames)} {...rest} >{children}</a>
-=======
-      <a className={cx(classnames)} {...rest} target="_blank" rel="noopener noreferrer">{children}</a>
->>>>>>> 4ff0c8356a62e5cb6fdd9c0c3c524eb142d215bd
     </React.Fragment>
   );
 }
