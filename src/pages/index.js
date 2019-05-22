58df4f930ad5b4a7dd210c0aa267589aae0597f9
Ozzy Cardenas <ocardenas789@me.com>
2019-05-21 21:18:38 -0700

    added hotel information, UI bug fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@ import Layout from "../components/layout";
 
 import Hero from "../components/Hero";
 import RegistrationPage from "../components/Registry";
+import Hotel from "../components/Hotel";
 
 const IndexPage = () => (
   <Layout>
     <Hero />
     <RegistrationPage />
+    <Hotel />
   </Layout>
 )
 
